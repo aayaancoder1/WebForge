d2b6a58b1d66a1f727c2e0f27ac9a57c2a236d0d
aayaancoder1 <aayaan.shahwaiz@gmail.com>
2026-03-07 11:02:56 +0530

    Button Demo

---
+++ js_clg/add.js
@@ -0,0 +1,11 @@
+function add() {
+	var n1 = prompt("Enter first number", "12");
+	var n2 = prompt("Enter second number", "14");
+	//alert("The sum of the two numbers is: " + (n1 + n2));
+	document.writeln("The sum of the two numbers is: " + (parseInt(n1) + parseInt(n2)));
+	//let text;
+	// if (confirm("Click on click button" + " yes or no")) {
+	// 	text = "User clicked OK";
+	// 	alert(text);	
+	// }
+}
